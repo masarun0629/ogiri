6092a5861edcbfc1e1d7b3ca6c93019b381d194f
masarun0629 <o8058642424@gmail.com>
2022-11-08 22:41:11 +0900

    ビューの編集など諸々

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,23 @@
 class QuestionsController < ApplicationController
   def index
     @questions = Question.all
+  end
+  
+  def new
+    @question = Question.new
+  end
+  
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end  
+
+  private
+  def question_params
+    params.require(:question).permit(:question).merge(user_id: current_user.id)
+  end
 end
