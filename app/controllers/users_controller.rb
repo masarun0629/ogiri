ddcab48e096c1f1633c8a456eb230801bd54a18e
masarun0629 <o8058642424@gmail.com>
2022-11-18 02:12:15 +0900

    ビューの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
   
+  def index
+    @users = User.all
+  end  
+  
   def show
     @users = User.all
     @user = User.find(params[:id])
-    @user_answers_likes = @user.answers.includes(:liked_users)
+    @user_answers = @user.answers
+    @likes_count = 0
+    @user_answers.each do |answer|
+      @likes_count += answer.likes.count
+    end
   end
 
 end
