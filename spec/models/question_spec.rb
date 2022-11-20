79d47083232e14e175c9e54890ba5d193f79648f
masarun0629 <o8058642424@gmail.com>
2022-11-20 17:34:35 +0900

    クエスチョンモデルのテストコード

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @question = FactoryBot.build(:question)
+  end
+  
+  describe 'お題投稿機能' do
+  
+    context '投稿できる場合' do
+      
+      it 'すべての値が正確に入力されていれば投稿できる' do
+        expect(@question).to be_valid
+      end
+      
+      it '画像がなくても投稿できる' do
+        @question.image = nil
+        expect(@question).to be_valid
+      end
+      
+    end
+    
+    context '投稿できない場合' do
+      
+      it "questionが空では登録できない" do
+        @question.question = ''
+        @question.valid?
+        expect(@question.errors.full_messages).to include("Question translation missing: ja.activerecord.errors.models.question.attributes.question.blank")
+      end
+      
+      it "questionが81文字以上では登録できない" do
+        @question.question = "#{"あ"*81}"
+        @question.valid?
+        expect(@question.errors.full_messages).to include("Question translation missing: ja.activerecord.errors.models.question.attributes.question.too_long")
+      end
+      
+      it 'userが紐付いていないと保存できない' do
+        @question.user = nil
+        @question.valid?
+        expect(@question.errors.full_messages).to include("User translation missing: ja.activerecord.errors.models.question.attributes.user.required")
+      end
+      
+    end
+  
+  end
+    
 end
